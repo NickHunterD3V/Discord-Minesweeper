d221648d00e7865b51a4a516e7dc328cfadaa1f6
Nicolas Ramos <95194980+NickHunterD3V@users.noreply.github.com>
2022-10-27 02:22:56 -0300

    Add files via upload

---
+++ source/commands/developers/eval.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'eval',
+    code: `
+#(newArray whitelist | 757006394531512390,786623348435976213)
+#(if #(arrayIncludes whitelist | #(author)) != true | #(send you're dumb) #(break))
+
+#(eval #(args all)) 
+`
+}
+
+/*&eval #(createSlashCommand math | Math commands | 
+	#(addSubcommand matrix | Resolves a matrix with given values and expression. | 
+	  #(addNumberOption lines | Amount of matrix lines. | 1 || true) 
+	  #(addNumberOption columns | Amount of matrix columns. | 1 || true))
+      #(addStringOption expression | The expression of matrix values. ||| true)
+)*/
