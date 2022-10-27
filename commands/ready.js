8183fefa7b20ad52e6bc4e710de7a55a0a479c8a
Nicolas Ramos <95194980+NickHunterD3V@users.noreply.github.com>
2022-10-27 02:21:50 -0300

    Delete commands directory

--- commands/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    type: 'ready',
-    code: `
-#(log HyteScript discord client: Logged in client: #(client name))
-#(newArray gs | #(allGuilds))
-#(arrayMap gs | #(allGuildMembers | {arrElement}))
-
-`
-}
